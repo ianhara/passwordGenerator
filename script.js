4e107a483e97b583d99d283f7cde4566332305f0
ianhara <ianhara97@gmail.com>
2024-02-26 20:40:36 -0500

    moved while loop so that page doesnt break after password is created and added comments for clarity

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-var string = "adfdl";
-
-var randomchar = Math.floor(Math.random() * string.length);
-
-console.log(randomchar);
-
-console.log(string[randomchar]);
-
-for (var i = 0; i < string.length; i++) {
-  console.log(i, "loop");
-}
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -27,36 +15,49 @@ generateBtn.addEventListener("click", writePassword);
 //elements to add to string
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = uppercase.toLowerCase();
-var special = "!@#$%^&*()-+=<>,."
+var special = "!@#$%^&*()-+=<>,.";
 var number = "123456789";
+
+//the string that we will add all of the characters that are chosen
 var passwordString = "";
-var passLength = false;
 console.log(uppercase, lowercase);
 
 function generatePassword() {
+  //set length variable to false until valid input is gained
+  var passLength = false;
+
   //using a while loop to make sure that a valid length is entered
-  while(!passLength){
-  var passwordLength = prompt(
-    "Enter the ength of your password 8-128 characters"
-  );
-  if (!isNaN(passwordLength) && passwordLength>=8 && passwordLength<=128){
-    passLength = true;
+  while (!passLength) {
+    var passwordLength = prompt(
+      "Enter the ength of your password 8-128 characters"
+    );
+    //if passwordlength is valid exit
+    if (
+      !isNaN(passwordLength) &&
+      passwordLength >= 8 &&
+      passwordLength <= 128
+    ) {
+      passLength = true;
+    } else alert("invalid length, please enter a value from 8-128");
   }
-  else alert("invalid length, please enter a value from 8-128")
-  }
- 
+
   //variables to add to string
   var includeUppercase = confirm("Uppercase letters");
   var includeLowercase = confirm("Lowercase letters");
   var includeSpecial = confirm("Special Characters");
   var includeNumber = confirm("numbers");
 
-  //if all false return 
-if(!includeLowercase && !includeNumber && !includeSpecial && !includeUppercase){
-  alert("you must select yes for at least one")
-  generatePassword()
-  return
-}
+  //if all false return otherwise concatenate desired chars
+  if (
+    !includeLowercase &&
+    !includeNumber &&
+    !includeSpecial &&
+    !includeUppercase
+  ) {
+    alert("you must select yes for at least one");
+    generatePassword();
+    return;
+  }
   if (includeUppercase) {
     passwordString += uppercase;
     console.log(passwordString);
@@ -77,14 +78,15 @@ if(!includeLowercase && !includeNumber && !includeSpecial && !includeUppercase){
     console.log(passwordString);
   }
 
-  var password = ""
+  //empty string to be set
+  var password = "";
 
+  //loop initalizes the elements of final "password" with random chars taken from pass String
   for (var i = 0; i < passwordLength; i++) {
-    var randomchar = Math.floor(Math.random() * passwordString.length );
-    password+= passwordString[randomchar]
+    var randomchar = Math.floor(Math.random() * passwordString.length);
+    password += passwordString[randomchar];
     console.log(password, "loop");
   }
-  return password
+  //displays the password in the box
+  return password;
 }
-
-
